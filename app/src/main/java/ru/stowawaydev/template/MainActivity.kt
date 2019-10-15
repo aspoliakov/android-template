4bf90697d7dfd7bc7971fb20d22d0f7dadb0a2b3
aspoliakov <poliakovas.main@gmail.com>
2019-10-15 22:27:20 +0300

    gradle dependencies configured. Project has migrated to androidx

--- app/src/main/java/ru/stowawaydev/template/MainActivity.kt
+++ app/src/main/java/ru/stowawaydev/template/MainActivity.kt
@@ -1,7 +1,7 @@
 package ru.stowawaydev.template
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
